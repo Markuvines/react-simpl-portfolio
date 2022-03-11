5bf23ac0586bbf519120c63f68a38d9683909f60
Markuvines <shinigami_master@mail.ru>
2022-03-11 17:16:36 +0300

    added 'Home' component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import Header from './components/Header/Header';
 import './reset.css'
 import './style.css'
-import mySelf from './image/self.jpg'
-import menu from './image/menu.svg'
-
 import { useState } from 'react';
 import MobileMenu from './components/MobileMenu/MobileMenu';
+import Home from './components/Home/Home';
 
 function App() {
   const [lang, setLang] = useState('eng')
@@ -16,19 +14,7 @@ function App() {
         <MobileMenu mobileMenu={mobileMenu} setMobileMenu={setMobileMenu} lang={lang} setLang={setLang}/>
         <Header lang ={lang}/>
         <main>
-          <div className='home'>
-            <div className='home__content _container'>
-              <div className='home__title'>
-                <span className='home__name'>Denis Novik</span>
-                <span className='home__prof'>UX | UI designer 24 years old, Minsk</span>
-                <span className='home__langToggle'> 
-                  <span className={`home__lang ${lang==='ru' && 'home__lang_active'}`} onClick={() => setLang('ru')}>RU</span> | <span className={`home__lang ${lang==='eng' && 'home__lang_active'}`} onClick={() => setLang('eng')}>ENG</span>
-                </span>
-              </div>
-              <img className='home__menu' src={menu} alt="menu" onClick={() => {setMobileMenu(!mobileMenu)}}/>
-              <img className='home__image' src={mySelf} alt="mySelf"/>
-            </div>
-          </div>
+          <Home mobileMenu={mobileMenu} setMobileMenu={setMobileMenu} lang={lang} setLang={setLang}/>
         </main>
       </div>
     </>
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import mySelf from '../../image/self.jpg'
+import menu from '../../image/menu.svg'
+import './Home.css'
+
+const Home = ({mobileMenu, setMobileMenu, lang, setLang}) => {
+  return (
+    <div className='home'>
+      <div className='home__content _container'>
+      {(() => {
+        switch (lang) {
+          case 'ru': 
+            return(
+              <div className='home__title'>
+                <span className='home__name'>Денис Новик</span>
+                <span className='home__prof'>UX | UI дезайнер 24 года, Минск</span>
+                <span className='home__langToggle'> 
+                  <span className={`home__lang ${lang==='ru' && 'home__lang_active'}`} onClick={() => setLang('ru')}>РУС</span> | <span className={`home__lang ${lang==='eng' && 'home__lang_active'}`} onClick={() => setLang('eng')}>АНГЛ</span>
+                </span>
+              </div>
+            )
+          case 'eng':
+            return(
+              <div className='home__title'>
+          <span className='home__name'>Denis Novik</span>
+          <span className='home__prof'>UX | UI designer 24 years old, Minsk</span>
+          <span className='home__langToggle'> 
+            <span className={`home__lang ${lang==='ru' && 'home__lang_active'}`} onClick={() => setLang('ru')}>RU</span> | <span className={`home__lang ${lang==='eng' && 'home__lang_active'}`} onClick={() => setLang('eng')}>ENG</span>
+          </span>
+        </div>
+            )
+        }
+      })()}
+        <img className='home__menu' src={menu} alt="menu" onClick={() => {setMobileMenu(!mobileMenu)}}/>
+        <img className='home__image' src={mySelf} alt="mySelf"/>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
