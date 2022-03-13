3531e00f8473e41ef2dc49cbf52b7be930a9d76a
Markuvines <shinigami_master@mail.ru>
2022-03-13 14:39:19 +0300

    Added 'AboutMe' component

--- src/App.js
+++ src/App.js
@@ -4,20 +4,22 @@ import './style.css'
 import { useState } from 'react';
 import MobileMenu from './components/MobileMenu/MobileMenu';
 import Home from './components/Home/Home';
+import AboutMe from './components/AboutMe/AboutMe';
+import Skills from './components/Skills/Skills';
 
 function App() {
   const [lang, setLang] = useState('eng')
   const [mobileMenu, setMobileMenu] = useState(false);
   return (
-    <>
-      <div className='wrapper'>
-        <MobileMenu mobileMenu={mobileMenu} setMobileMenu={setMobileMenu} lang={lang} setLang={setLang}/>
-        <Header lang ={lang}/>
-        <main>
-          <Home mobileMenu={mobileMenu} setMobileMenu={setMobileMenu} lang={lang} setLang={setLang}/>
-        </main>
-      </div>
-    </>
+    <div className='wrapper'>
+      <MobileMenu mobileMenu={mobileMenu} setMobileMenu={setMobileMenu} lang={lang} setLang={setLang}/>
+      <Header lang ={lang}/>
+      <main>
+        <Home mobileMenu={mobileMenu} setMobileMenu={setMobileMenu} lang={lang} setLang={setLang}/>
+        <AboutMe lang={lang}/>
+        <Skills lang={lang}/>
+      </main>
+    </div>
   );
 }
 
---
+++ src/components/AboutMe/AboutMe.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import './AboutMe.css'
+
+const AboutMe = ({lang}) => {
+  return (
+    <div className='aboutMe'>
+      <div className='aboutMe__content _container'>
+      {(() => {
+        switch (lang) {
+          case 'eng': return(
+            <>
+              <h2 className='aboutMe__title'>About me</h2>
+              <p className='aboutMe__discription'>
+                Hi, I'm Denis – UX/UI designer from Minsk. <br/>
+                I'm interested in design and everything connected with it. 
+                <br/>
+                <br/>
+                I'm studying at courses "Web and mobile design <br/>
+                interface" in IT-Academy.
+                <br/>
+                <br/>
+                Ready to implement excellent projects <br/>
+                with wonderful people.
+              </p>
+            </>
+          );
+          case 'ru': return(
+            <>
+              <h2 className='aboutMe__title'>Обо мне</h2>
+              <p className='aboutMe__discription'>
+                Привет, я Денис – UX/UI дизайнер из Минска. <br/>
+                Я интересуюсь дизайном и всем, что с этим связано. 
+                <br/>
+                <br/>
+                Я обучался на курсах "Веб и мобильные дизайн <br/>
+                интерфейсы" в IT-Академии.
+                <br/>
+                <br/>
+                Готов создавать прекрасные проекты <br/>
+                с замечательными людьми.
+              </p>
+            </>
+          )
+        }
+      })()}
+      </div>
+    </div>
+  );
+};
+
+export default AboutMe;
